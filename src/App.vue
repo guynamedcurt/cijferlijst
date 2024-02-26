504743b553fbfd77308dea759be3f92a2464bdd9
guynamedcurt <jaccoadriaan@gmail.com>
2024-02-26 09:53:48 +0100

    Testing to where this goes??

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>
+    Cijferlijst
+  </h1>
+  <div>
+    <table>
+      <tr><td bgcolor="gray">Examen</td><td bgcolor="gray">Cijfer</td></tr>
+      <examen-lijst v-for="(e,index) in cijfers" @veranderId="veranderId" @verwijderId="verwijderId" :key="index"
+            :index = "index"
+            :examen = "e.examen"
+            :nummer = "e.nummer"
+            >
+      </examen-lijst>
+    </table>
+    <form @submit.prevent="addCijfer()">
+      <td>
+        <label for="examenInvul">Examen:</label>
+        <input id="examenInvul" type="text" v-model="newExamenInvul" required/>
+      </td>
+      <td>
+        <label for="cijferInvul">Cijfer:</label>
+        <input id="cijferInvul" type="text" v-model="newCijferInvul"/>
+        <button type="submit">Submit</button>
+      </td>
+    </form>
+    <br>
+    <span>Gemiddelde: {{ average }} ({{ this.cijfers.filter(el => el.nummer != null).length }} cijfers)</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ExamenLijst from './components/Examen'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ExamenLijst
+  },
+  methods: {
+    veranderId(index, nummer){
+      this.cijfers[index].nummer= null;
+      if (isNaN(parseFloat(nummer))) {
+        this.cijfers[index].nummer = null;
+      } else {
+        this.cijfers[index].nummer = parseFloat(nummer);
+      }
+    },
+    addCijfer() {
+      const newCijfer = {
+        examen: this.newExamenInvul,
+        nummer: parseFloat(this.newCijferInvul),
+      };
+
+      console.log(this.newExamenInvul + " en " + this.newCijferInvul);
+      this.cijfers.push(newCijfer);
+      this.newExamenInvul = "";
+      this.newCijferInvul = "";
+    },
+    verwijderId: function(index){
+      console.log(index + "Werkt!!");
+      this.cijfers.splice(index, 1);
+    }
+  },
+  data () {
+    return {
+      cijfers: [{ examen:"Frontend Development", nummer:null},
+          {examen:"Webdevelopment", nummer:7.5},
+          {examen:"Mobile Application Development", nummer:4.5},
+          {examen:"Stage 1", nummer:6.1},
+          {examen:"Stage 2", nummer:null}]
+    }
+  },
+  computed: {
+    average: function computeAverage() {
+      console.log(this.cijfers.reduce((sum, e) => sum + e.nummer, 0), this.cijfers.filter(el => el.nummer != null).length, this.cijfers.reduce((sum, e) => sum + e.nummer, 0) / this.cijfers.filter(el => el.nummer != null).length, Math.round(this.cijfers.reduce((sum, e) => sum + e.nummer, 0) / this.cijfers.filter(el => el.nummer != null).length * 10) / 10);
+      console.log (this.cijfers.reduce((sum, e) => sum + e.nummer, 0) / this.cijfers.filter(el => el.nummer != null).length);
+      return Math.round (this.cijfers.reduce((sum, e) => sum + e.nummer, 0) / this.cijfers.filter(el => el.nummer != null || el.nummer != isNaN).length * 10) / 10;
+    },
   }
 }
 </script>
@@ -22,5 +86,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.success {
+  background-color: green;
+}
+.failure {
+  background-color: red;
+}
+.blank {
+  background-color: darkgray;
+}
+.tableWidth {
+  width: 15%;
 }
 </style>
---
+++ src/components/Examen.vue
@@ -0,0 +1,32 @@
+<template>
+    <tr :id="index" :class="classGrade">
+        <td style="width:20vw">{{ examen }}</td><td style="width:5vw"><input type="text" :value="nummer" @keyup='verander'/></td>
+        <td style="width:1vw"><button @click="verwijder">X</button></td>
+    </tr>
+</template>
+<script>
+    export default {
+        name: 'ExamenLijst', // element <Examen>
+        props: ['index', 'examen', 'nummer'], //gegevens van het element
+        methods:{
+            verwijder: function(){
+                this.$emit('verwijderId', this.index)
+            },
+            verander($event){
+                console.log('verander', this.index, this.nummer, '>', $event.target.value)
+                if ($event.target.value != undefined || $event.target.value != null || $event.target.value != '') {
+                    this.$emit('veranderId', this.index, $event.target.value)       //vuur event af
+                }
+            }
+        },
+        computed: {
+            classGrade() {
+                return {
+                    'success': this.nummer >= 5.5 && this.nummer <= 10.0,
+                    'failure': this.nummer >= 0.1 && this.nummer <= 5.4,
+                    'blank': this.nummer == '' || this.nummer == null
+                }
+            }
+        }
+    }
+</script>
